255c3c4a0b3d9054483246cb359e6bd0c89a5b4d
Tiani Edwards <tianiedwards@Tianis-MacBook-Pro.local>
2023-01-19 20:41:18 -0600

    Adding code to files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -36,7 +36,7 @@ let cityData = cities;
   // Loop through the cities array and create one marker for each city.
   cityData.forEach(function(city) {
     console.log(city)
-    L.marker(city.location,{
+    L.circleMarker(city.location,{
         radius: city.population/100000
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population "+ city.population.toLocaleString() + "</h3>")

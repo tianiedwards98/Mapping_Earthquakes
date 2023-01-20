9dbebb8304132b1eefe3a56312427c7c37ee17db
Tiani Edwards <tianiedwards@Tianis-MacBook-Pro.local>
2023-01-19 21:12:16 -0600

    Adding code for mapping GeoJSON data

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,10 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
-//let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-//let map = L.map('mapid').setView([37.6214, -122.3790], 5);
 // Create the map object with center at the San Francisco airport.
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
@@ -18,48 +14,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map); 
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
-
-// L.circleMarker([34.0522, -118.2437],{
-//     radius: 300,
-//     color: "black",
-//     fillColor: "ffffa1"
-// }).addTo(map);
-
-// Get data from cities.js
-let cityData = cities;
-
-  // Loop through the cities array and create one marker for each city.
-//   cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location,{
-//         radius: city.population/100000
-//     })
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population "+ city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-// });
-
-
-// Coordinates for each point to be used in the line.
-let line = [
-    [33.9416, -118.4085],
-    [37.6214, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-// Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-//     color: "yellow"
-//   }).addTo(map);
-
-// Mapping GeoJSON Point
-
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -81,6 +35,20 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.city + "</h2>");
+  }
+
+}).addTo(map);
+
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+   }
+}).addTo(map);
